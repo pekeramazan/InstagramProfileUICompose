b9a055e4ae4788313b1005bfaf5beeb14f046268
ramazan.peker <pekeramazan@gmail.com>
2023-11-30 23:54:01 +0300

    top bar added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.ramazanpeker.instagramprofileuicompose"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.ramazanpeker.instagramprofileuicompose"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
